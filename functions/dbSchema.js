782f4a96334bd6c54a2844b16218d78acb9b7711
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-04-21 23:33:09 -0400

    Signup Route for user registration implemented

---
+++ functions/dbSchema.js
@@ -0,0 +1,18 @@
+let db = {
+    babbles: [
+        {
+            userHandle: 'baby1',
+            body: 'this is the babble body',
+            createdAt: '2020-04-21T23:14:29.112Z',
+            likeCount: 5,
+            commentCount: 2
+        },
+        {
+            userHandle: 'baby2',
+            body: 'this is again babble body',
+            createdAt: '2020-04-21T23:14:29.112Z',
+            likeCount: 2,
+            commentCount: 3
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
+const firebase = require('firebase');
 
 admin.initializeApp();
 const app = express();
+
+const config = {
+    apiKey: "AIzaSyA52n7t4kupgX240-9_1qepWrtyUf7ofB8",
+    authDomain: "socialbabies.firebaseapp.com",
+    databaseURL: "https://socialbabies.firebaseio.com",
+    projectId: "socialbabies",
+    storageBucket: "socialbabies.appspot.com",
+    messagingSenderId: "1050529246750",
+    appId: "1:1050529246750:web:2c65cd2386cf6f74e9c80f"
+};
+firebase.initializeApp(config);
+
 
 exports.api = functions.https.onRequest(app);
 
@@ -18,11 +31,17 @@ app.get('/babbles', (req, res) => {
     admin
         .firestore()
         .collection('babbles')
+        .orderBy('createdAt', 'desc')
         .get()
         .then(data => {
             let babbles = [];
             data.forEach((doc) => {
-                babbles.push(doc.data());
+                babbles.push({
+                    babbleId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
             });
             return res.json(babbles);
         })
@@ -33,7 +52,7 @@ app.post('/babble', (req, res) => {
     const newBabble = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin
@@ -46,5 +65,29 @@ app.post('/babble', (req, res) => {
         .catch(err => {
             res.status(500).json({error: `Something went wrong`});
             console.log(err);
+        });
+});
+
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    };
+    console.log(newUser);
+
+    // To-do validate data
+
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(data => {
+            return res.status(201)
+                    .json({message: `user ${data.user.uid} signed up successfully`});
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({error: err.code});
         });
 });
