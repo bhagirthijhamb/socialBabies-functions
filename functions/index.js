25833002f366e20dd607271436a5220e0e6438aa
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-10-14 19:37:41 -0400

    added Validation code for Signup Route(for email, password, confirmpassword and handle)

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const express = require('express');
 const firebase = require('firebase');
-
-admin.initializeApp();
-const app = express();
+const express = require('express');
 
 const config = {
     apiKey: "AIzaSyA52n7t4kupgX240-9_1qepWrtyUf7ofB8",
@@ -15,17 +12,58 @@ const config = {
     messagingSenderId: "1050529246750",
     appId: "1:1050529246750:web:2c65cd2386cf6f74e9c80f"
 };
-firebase.initializeApp(config);
 
+firebase.initializeApp(config);
+admin.initializeApp();
+const app = express();
 
 exports.api = functions.https.onRequest(app);
+const db = admin.firestore();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+// exports.getBabbles = functions.https.onRequest((req, res) => {
+//     admin
+//     .firestore()
+//     .collection('babbles')
+//     .get()
+//     .then(data => {
+//         let babbles = [];
+//         data.forEach(doc => {
+//             babbles.push(doc.data());
+//         });
+//         return res.json(babbles);
+//     })
+//     .catch(err => console.error(err));
+// })
+
+// exports.createBabble = functions.https.onRequest((req, res) => {
+//     if(req.method !== 'POST'){
+//         return res.status(400).json({ error: 'This method is not allowed'})
+//     }
+//     const newBabble = {
+//         body : req.body.body,
+//         userHandle: req.body.userHandle,
+//         createdAt: admin.firestore.Timestamp.fromDate(new Date())
+//     };
+
+//     admin
+//         .firestore()
+//         .collection('babbles')
+//         .add(newBabble)
+//         .then(doc => {
+//             return res.json({message: `document ${doc.id} created successfully. Yuhoo...`})
+//         })
+//         .catch(err => {
+//             res.status(500).json({error: `Oops... Something went wrong.`});
+//             console.error(err);
+//         })
+// })
 
 app.get('/babbles', (req, res) => {
     admin
@@ -68,6 +106,22 @@ app.post('/babble', (req, res) => {
         });
 });
 
+const isEmpty = string => {
+    if(string.trim() === '') return true;
+    else return false;
+}
+
+const isEmail = (email) => {
+    const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    // const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // return re.test(String(email).toLowerCase());
+
+    if(email.match(regEx)) return true;
+    else return false;
+}
+
+// Signup Route
 app.post('/signup', (req, res) => {
     const newUser = {
         email: req.body.email,
@@ -75,19 +129,75 @@ app.post('/signup', (req, res) => {
         confirmPassword: req.body.confirmPassword,
         handle: req.body.handle
     };
-    console.log(newUser);
+
+    let errors = {};
+
+    // validate email
+    if(isEmpty(newUser.email)){
+        errors.email = 'Email must not be empty'
+    } else if(!isEmail(newUser.email)){
+        errors.email = 'Must be a valid email address'
+    }
+
+    // validate password
+    if(isEmpty(newUser.password)) errors.password = 'Must not be empty';
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+    if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
 
     // To-do validate data
-
-    firebase
-        .auth()
-        .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if(doc.exists){
+                return res.status(400).json({ handle: 'This handle is already taken'})
+            } else {
+                return firebase
+                  .auth()
+                  .createUserWithEmailAndPassword(
+                    newUser.email,
+                    newUser.password
+                  );
+            }
+        })
         .then(data => {
-            return res.status(201)
-                    .json({message: `user ${data.user.uid} signed up successfully`});
+            userId = data.user.uid;
+            return data.user.getIdToken()
+        })
+        .then(idToken => {
+            token = idToken;
+            // return res.status(201).json({ token });
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                // userId: userId
+                userId
+            };
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() => {
+            return res.status(201).json({ token });
         })
         .catch(err => {
-            console.error(err);
-            return res.status(500).json({error: err.code});
-        });
+            console.error(err);    
+            if(err.code === "auth/email-already-in-use"){
+                return res.status(400).json({ email: "Email already in use"});
+            } else {
+                return res.status(500).json({ error: err.code });
+            }
+        })
+
+    // firebase
+    //     .auth()
+    //     .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    //     .then(data => {
+    //         return res.status(201)
+    //                 .json({message: `user ${data.user.uid} signed up successfully`});
+    //     })
+    //     .catch(err => {
+    //         console.error(err);
+    //         return res.status(500).json({error: err.code});
+    //     });
 });
