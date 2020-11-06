8898749a9b3076881dcd2c29513b1bda751078b5
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-11-06 10:27:47 -0500

    implemented funtionality to like a babble

--- functions/routes/babbleRoutes.js
+++ functions/routes/babbleRoutes.js
@@ -39,7 +39,10 @@ router.post('/babble', FBAuth, (req, res) => {
     body: req.body.body,
     // userHandle: req.body.userHandle,
     userHandle: req.user.handle,
+    useImage: req.user.imageUrl,
     createdAt: new Date().toISOString(),
+    likeCount: 0,
+    commentCount: 0,
   };
 
   admin
@@ -47,7 +50,10 @@ router.post('/babble', FBAuth, (req, res) => {
     .collection("babbles")
     .add(newBabble)
     .then((doc) => {
-      return res.json({ message: `document ${doc.id} created successfully.` });
+      const resBabble = newBabble;
+      resBabble.babbleId = doc.id;
+      // return res.json({ message: `document ${doc.id} created successfully.` });
+      return res.json(resBabble);
     })
     .catch((err) => {
       res.status(500).json({ error: `Something went wrong` });
@@ -105,6 +111,81 @@ router.post('/:babbleId/comment', FBAuth, (req, res) => {
     .catch(err => {
       console.log(err);
       res.status(500).json({ error: 'Something went wrong' });
+    })
+})
+
+router.get('/:babbleId/like', FBAuth, (req, res) => {
+  const babbleDocument = db.doc(`/babbles/${req.params.babbleId}`);
+  const likeDocument = db.collection('likes').where('userHandle', '==', req.user.handle).where('babbleId', '==', req.params.babbleId).limit(1);
+  let babbleData;
+  babbleDocument.get()
+    .then(doc => {
+      if(doc.exists){
+        babbleData = doc.data();
+        babbleData.babbleId = doc.id;
+        return likeDocument.get();
+      }
+      else{
+        return res.status(404).json({ error: 'Babble not found.'});
+      }
+    })
+    .then(data => {
+      if(data.empty){
+        return db.collection('likes').add({
+          babbleId: req.params.babbleId,
+          userHandle: req.user.handle
+        })
+        .then(() => {
+          babbleData.likeCount++;
+          return babbleDocument.update({ likeCount: babbleData.likeCount})
+        })
+        .then(() => {
+          return res.json(babbleData);
+        })
+      }
+      else {
+        return res.status(400).json({ error: 'Babble already liked.' });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    })
+})
+
+router.get('/:babbleId/unlike', FBAuth, (req, res) => {
+  const babbleDocument = db.doc(`/babbles/${req.params.babbleId}`);
+  const likeDocument = db.collection('likes').where('userHandle', '==', req.user.userHandle).where('babbleId', '==', req.params.babbleId).limit(1);
+  let babbleData;
+  babbleDocument.get()
+    .then(doc => {
+      if(doc.exists){
+        babbleData = doc.data();
+        babbleData.babbleId = doc.id;
+        return likeDocument.get();
+      }
+      else{
+        return res.status(404).json({ error: 'Babble not found.'});
+      }
+    })
+    .then(data => {
+      if(data.empty){
+        return res.status(400).json({ error: 'Babble not liked.' });
+      }
+      else {
+        return db.doc(`/likes/${data.docs[0].data().id}`).delete()
+          .then(() => {
+            babbleData.likeCount--;
+            return babbleDocument.update({ likeCount: babbleData.likeCount });
+          })
+          .then(() => {
+            return res.json(babbleData);
+          })
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
     })
 })
 
